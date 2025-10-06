cc1db4e1eb60a9c35ca420454a9f4c7d6578fcb5
jossinuddin <jossinuddin@gmail.com>
2025-10-06 11:15:41 +0000

    Nip and tuck around the codebase

--- habitsmith/__init__.py
+++ habitsmith/__init__.py
@@ -2,3 +2,4 @@ from .storage import track, list_days, read_day
 from .render import render_weekly_report
 
 __all__ = ["track", "list_days", "read_day", "render_weekly_report"]
+# tweak 2025-10-06T11:15:41.335674
