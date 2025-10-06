9ee46030e776e602520f15ea98b0224e96529a76
jossinuddin <jossinuddin@gmail.com>
2025-10-06 11:23:10 +0000

    Keep habitsmith/__init__.py tidy (no logic changes)

--- habitsmith/__init__.py
+++ habitsmith/__init__.py
@@ -3,3 +3,5 @@ from .render import render_weekly_report
 
 __all__ = ["track", "list_days", "read_day", "render_weekly_report"]
 # tweak 2025-10-06T11:21:51.879390
+
+# autosave 2025-10-06T11:23:10.262807
